dda97b3c3f972b37ce7c962fd7fe3afda9e366b7
Adrian <adrianrodriguezotero@hotmail.com>
2019-04-22 12:26:05 +0200

    'pub'

--- src/app/projects/projects/filter-form/filter-form.component.ts
+++ src/app/projects/projects/filter-form/filter-form.component.ts
@@ -6,13 +6,20 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./filter-form.component.css']
 })
 export class FilterFormComponent implements OnInit {
+  public crit: String;
   public project: { id: number, name: String };
   @Output() public search = new EventEmitter();
+  @Output() public reset = new EventEmitter();
   constructor() {
-    this.project = { id: 0, name: ''};
+
+
   }
 
   ngOnInit() {
+    this.project = { id: null, name: ''};
   }
-
+  public res() {
+    this.project = { id: null, name: '' };
+    this.crit = "";
+}
 }
--- src/app/projects/projects/projects.component.ts
+++ src/app/projects/projects/projects.component.ts
@@ -9,10 +9,23 @@ import { environment } from '../../../environments/environment';
 export class ProjectsComponent implements OnInit {
   public projects = environment.projects;
 
-  constructor() {
-      }
+  constructor() {}
   ngOnInit() {}
-  public onSearch(casa: { id: number, name: String }) {
-    console.log( "IT WORKS" + casa.id + casa.name );
+  public onSearch( p: any ) {
+    this.projects = environment.projects;
+    if (p.crit != null && p.crit != '') {
+      if (p.crit == 'id') {
+        this.projects = this.projects.filter(pr => pr.id == p.project.id);
+        return;
+      } else if(p.crit == 'name'){
+        this.projects = this.projects.filter(pr => pr.name.includes(p.project.name));
+        return;
+      }
+    }
+    return;
+  }
+  public onReset() {
+    this.projects = environment.projects;
+    return;
   }
 }
